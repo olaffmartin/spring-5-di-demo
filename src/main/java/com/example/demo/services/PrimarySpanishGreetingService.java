58773e956f7a770453d25c46fac6eac5dfc5e5d3
Olaff Molero <olaffmartin@gmail.com>
2017-09-24 11:35:03 -0500

    add @Profile to the project

---
+++ src/main/java/com/example/demo/services/PrimarySpanishGreetingService.java
@@ -0,0 +1,17 @@
+package com.example.demo.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+@Profile("es")
+public class PrimarySpanishGreetingService implements GreetingService {
+
+	@Override
+	public String sayGreeting() {
+		return "Hola - Primer Servicio de Saludo";
+	}
+
+}
